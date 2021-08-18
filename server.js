9a1ebef0d602dccc666d96c6fa291425ce2ff282
Kaung Htet Paing <kaunghtetpaing199@gmail.com>
2021-08-18 09:04:56 +0630

    class 2

--- server.js
+++ server.js
@@ -7,7 +7,9 @@ const path = require("path");
 const app = express();
 const mongoose = require("mongoose");
 const expressLayouts = require("express-ejs-layouts");
+
 const indexRouter = require("./routes/index");
+const authorRouter = require("./routes/authors");
 
 mongoose.connect(process.env.DATABASE_URL, {
 	useNewUrlParser: true,
@@ -18,6 +20,8 @@ const db = mongoose.connection;
 db.on("error", (error) => console.error(error));
 db.once("open", () => console.log("Connected to Database"));
 
+app.use(express.json());
+app.use(express.urlencoded());
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 app.set("layout", "layouts/layout");
@@ -27,4 +31,6 @@ app.use(express.static("public"));
 const port = process.env.PORT || 3000;
 
 app.use("/", indexRouter);
+app.use("/authors", authorRouter);
+
 app.listen(port, () => console.log(`Server running at port ${port}`));
